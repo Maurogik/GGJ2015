401675da3b8a52f67d7c7eeeb1ab119360af419a
Maurogik <gwenn.aubert@gmail.com>
2015-01-25 00:28:43 +0100

    added ambiant sounds

---
+++ Assets/ProjectData/Scripts/Tmp/AmbiantSoundMgr.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbiantSoundMgr : MonoBehaviour {
+
+    public AudioSource source;
+    public AudioClip cave;
+    public AudioClip corridor;
+    //public AudioClip walkWater;
+    
+    public void OnLocationChanged(){
+        switch (GameState.currentLocation) {
+        case GameState.Location.CAVE :
+            source.clip = cave;
+            break;
+        case GameState.Location.CORRIDOR :
+            source.clip = corridor;
+            break;
+        /*case GameState.Location.WATER :
+            walkSource.clip = walkWater;
+            break;*/
+        }
+        //walkSource.volume = 0.0f;
+        source.Play ();
+    }
+}
