5075ccb897223b8a6bb3429bcff6f367d4e70bd5
Maurogik <gwenn.aubert@gmail.com>
2015-01-24 21:48:50 +0100

    toto

--- Assets/ProjectData/Scripts/Movement/MovecharCtrl.cs
+++ Assets/ProjectData/Scripts/Movement/MovecharCtrl.cs
@@ -6,7 +6,7 @@ public class MovecharCtrl : MonoBehaviour {
     private CharacterController mCtrl;
     public float speed = 5.0f;
     public float rotationSpeed = 2.0f;
-    public float grabity = 9.8f;
+    public float grabity = 4.5f;
     
 
     // Use this for initialization
@@ -18,18 +18,19 @@ public class MovecharCtrl : MonoBehaviour {
 	void Update () {
         Vector3 move = new Vector3 (XBoxController.instance.GetLeftStick ().x, 0.0f, XBoxController.instance.GetLeftStick ().y);
         move = transform.TransformDirection (move);
-        move.y = -grabity;
-        if (XBoxController.instance.GetButtonA()) {
-            move.y = 10.0f * Time.deltaTime;
+        if (XBoxController.instance.GetButtonA () && mCtrl.isGrounded) {
+            move.y = 60.0f;
+        } else {
+            move.y = -grabity;
         }
-        //move.Normalize ();
+        move.Normalize ();
         float valX = XBoxController.instance.GetRightStick ().x * Time.deltaTime * rotationSpeed;
         float valY = XBoxController.instance.GetRightStick ().y * Time.deltaTime * rotationSpeed;
         //transform.Rotate( new Vector3( -rotX, rotY, 0.0f) );
         Vector3 lookAt = new Vector3 (valX * Time.deltaTime, valY * Time.deltaTime, 1.0f);
         lookAt *= rotationSpeed;
         transform.LookAt (transform.position + transform.TransformDirection (lookAt));
-        mCtrl.Move (move * speed);
+        mCtrl.Move (move * speed * Time.deltaTime);
 	}
 
     /*IEnumerator jump(){
