73007f4b4ed31101e55db07b8f1f6f9824505295
Momo <maugard.matthieu@gmail.com>
2015-01-24 15:13:19 +0100

    Rising sun

---
+++ Assets/ProjectData/Scripts/Tmp/RisingSun.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RisingSun : MonoBehaviour {
+
+	IEnumerator rise(Light light, float time){
+		float startTime = Time.time;
+		while (startTime + time > Time.time) {
+			light.transform.Rotate(Vector3.left, 100.0f/(Time.time - startTime));
+			yield return null; //skip to next frame
+		}
+	}
+
+	void OnRise(Light light) {
+		StartCoroutine(rise(light, 3600.0f));
+	}
+}
